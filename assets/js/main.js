8f3855f47558e4199d907548d7e32521d3cc3901
Jenseko <jenscode23@gmail.com>
2023-02-17 15:58:38 +0100

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+console.log("Yippie!");
+
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-1_js(2) ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+// document.write('<h1>"Hello World!"</h1>');
+// document.write('<p>"Have a nice day!"</p>');
+
+// let myText = "Hello again";
+// document.write(myText);
+
+// let vorName = "Name: Jens ";
+// let nachName = "Nachname: Kötschau";
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write(vorName);
+// document.write("<br>");
+// document.write(nachName);
+
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-2_js(2) ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+
+// document.getElementById("info").innerHTML
+
+// info.innerHTML = "<h1>Hello World</h1>";
+// info.innerHTML += "<h2>how are you?</h2>";
+
+// document.getElementById('container').innerHTML;
+
+// container.innerHTML = "<p>start of the element</p>";
+
+// document.write ("end of the element");
+
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-3_js(2) ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈
+
+// const gallery = document.getElementById("gallery");
+
+// gallery.innerHTML = "<figure><img src='https://unsplash.it/200/200' alt=' '/><figcaption>fig.1</figcaption></figure>";
+// gallery.innerHTML += "<figure><img src='https://unsplash.it/200/200' alt=' '/><figcaption>fig.2</figcaption></figure>";
+// gallery.innerHTML += "<figure><img src='https://unsplash.it/200/200' alt=' '/><figcaption>fig.3</figcaption></figure>";
+
+// gallery.style.display = "flex";
+
+
+// ≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ lvl_1-11_String Methods ≈≈≈≈≈≈≈≈≈
+
+let x = "Hello ";
+let y = "World<br>";
+
+let z = x + y;
+
+document.write(z);
+
+let n = x + " " + y;
+document.write(n);
+
+x += "World";
+
+document.write(x);
+
+let meinString = "Ich bin Erster!";
+
+meinString += " Ich komme auf Platz zwei!";
+
+console.log (meinString);
+
+
+
+
+
+
+// let x = "Hello "
+// let y = "World<br>"
+
+// n = x + " " + y
+
+// document.write (n)
+
+// x + "World"
+
+// document.write (x + "World")
+
+// let meinString = "Ich bin Erster"
+
+// meinString += " Ich komme auf Platz zwei"
+
+// console.log (meinString)
